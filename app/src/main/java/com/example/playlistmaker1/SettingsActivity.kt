a39a15c62da90a57694ec0ae742e100906de91aa
niketch1 <niketch1@yandex.ru>
2023-03-14 23:49:20 +0300

    Первый коммит

--- app/src/main/java/com/example/playlistmaker1/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker1/SettingsActivity.kt
@@ -1,11 +1,15 @@
 package com.example.playlistmaker1
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -16,5 +20,37 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
 
+
+        val share = findViewById<ImageView>(R.id.buttonShare)
+
+        share.setOnClickListener {
+            val message = getString(R.string.linkCourse)
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            val chooserIntent = Intent.createChooser(shareIntent, null)
+            startActivity(chooserIntent)
+        }
+
+        val support = findViewById<ImageView>(R.id.buttonSupport)
+
+        support.setOnClickListener {
+            val messageSup = getString(R.string.messageSup)
+            val theme = getString(R.string.theme)
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, "niketch1@yandex.ru")
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, theme)
+            supportIntent.putExtra(Intent.EXTRA_TEXT, messageSup)
+            startActivity(supportIntent)
+        }
+
+        val agreement = findViewById<ImageView>(R.id.buttonAgreement)
+
+        agreement.setOnClickListener {
+            val agreementIntent = Intent(Intent.ACTION_VIEW)
+            agreementIntent.data = Uri.parse(getString(R.string.offer))
+            startActivity(agreementIntent)
+        }
     }
 }
