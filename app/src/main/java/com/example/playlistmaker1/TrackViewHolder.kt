a98951f0b04183fdf291d3f49534dfc8925cf73d
niketch1 <niketch1@yandex.ru>
2023-04-06 11:14:18 +0300

    первый коммит

--- app/src/main/java/com/example/playlistmaker1/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker1/TrackViewHolder.kt
@@ -1,14 +1,21 @@
 package com.example.playlistmaker1
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.text.SimpleDateFormat
+import java.util.*
 
-class TrackViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+class TrackViewHolder(parent: ViewGroup) :
+        RecyclerView.ViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.track_item, parent, false)) {
 
     private val trackName: TextView = itemView.findViewById(R.id.trackName)
     private val trackImage: ImageView = itemView.findViewById(R.id.trackImage)
@@ -20,7 +27,7 @@ class TrackViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
         Glide.with(itemView)
             .load(item.artworkUrl100)
             .centerCrop()
-            .placeholder(R.drawable.sad)
+            .placeholder(R.drawable.placeholder)
             .transform(RoundedCorners(2))
             .into(trackImage)
 
@@ -34,6 +41,6 @@ class TrackViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
            trackName.text = name
         }
         artistName.text = item.artistName
-        trackTime.text = item.trackTime
+        trackTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(item.trackTimeMillis)
     }
 }
